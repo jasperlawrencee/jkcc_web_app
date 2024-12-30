722e27576c32478279c12608c5ce9cf85a9b3e84
jasperlawrencee <caballerojasper.55555@gmail.com>
2024-12-30 08:59:01 +0800

    hello world

---
+++ src/components/auth.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Auth = () => {
+  return (
+    <div>
+        <input placeholder='Email'/>
+        <input placeholder='Password'/>
+        <button>Sign Up</button>
+    </div>
+  )
+}
